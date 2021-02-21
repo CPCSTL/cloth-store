f81bab3ec7e281df45aa592725a16535991d768d
Muhammed Yadak <muhammedyadak@Muhammeds-MacBook-Pro.local>
2021-02-21 02:37:52 -0600

    adding items to cart drop down and showing quants on shopping icond

--- src/components/cart_icon/cart_icon.comp.jsx
+++ src/components/cart_icon/cart_icon.comp.jsx
@@ -5,21 +5,29 @@ import {ReactComponent as ShoppingIcon} from '../../assests/shopping-bag.svg';
 import {toggleCartHidden} from '../../redux/cart/cart.actions'
 
 
-const CartIcon = ({toggleCartHidden})=>(
+const CartIcon = ({toggleCartHidden , cartCount})=>(
     <div className='cart-icon'
     onClick ={()=>toggleCartHidden()}
     >
     <ShoppingIcon className='shopping-icon' />
-    <span className='item-count'>0</span>
+    <span className='item-count'>{cartCount}</span>
     </div>
 );
+
+
+const mapStateToProps =({cart:{cartItems}})=>({
+    cartCount: cartItems.reduce((accumalatedQuantity,cartItem)=>
+    (accumalatedQuantity + cartItem.quantity),0)
+})
+
+
 
 const mapDispatchToProps = dispatch=>({
     toggleCartHidden: ()=> dispatch(toggleCartHidden())
 })
 
 export default connect(
-    null,
+    mapStateToProps,
     mapDispatchToProps
     )(CartIcon)
 
